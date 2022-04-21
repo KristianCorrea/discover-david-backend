0edacf8acdfe609f190a1f6c97eef0bd87f32813
KristianCorrea <kristiangcorrea@live.com>
2022-04-21 18:02:20 -0400

    Attemmpt to fix forced port to now env port. (2)

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.get('/photos', async (req, res) => {
     return res.send(response.data);
 });
 
+const PORT = process.env.PORT || 7000;
 
-
-app.listen(80 || process.env.PORT, console.log(`Server running on port ${process.env.PORT}`));
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
